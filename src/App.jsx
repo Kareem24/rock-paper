4c14f1b56743594a5b315d69d642c3bc5aba64e4
kareem roqib <kareemolakunleh@gmail.com>
2023-09-20 08:27:58 +0100

    chore: setup the page layout , the mobile and the desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Mainpage from './components/Mainpage'
 
 function App() {
   return (
-    <div className='bg-blue-600 w-full h-screen pt-20'>
+    <div className='bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-start to-end w-full min-h-screen pt-20'>
      <Mainpage/>
     </div>
   )
--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import scissors from '../assets/images/icon-scissors.svg'
+import paper from '../assets/images/icon-paper.svg'
+import rock from '../assets/images/icon-rock.svg'
+import triangle from '../assets/images/bg-triangle.svg'
 const Mainpage = () => {
   return (
-    <div>
-      <header className='flex justify-between py-1 px-3 border-white rounded w-[50%] border m-auto items-center'>
+    <>
+      <header className='flex justify-between py-1 px-3 border-headerOutline rounded md:w-[60%] w-[90%] border m-auto items-center'>
         <div>
           <p className='font-bold text-xl capitalize text-white'>rock</p>
           <p className='font-bold text-xl capitalize text-white'>paper</p>
@@ -12,12 +14,33 @@ const Mainpage = () => {
         <div>
           <div className=' rounded-md bg-white  py-2 px-4 text-center'>
 
-          <p className='text-sm'>score</p>
-          <p className='font-bold text-4xl'>12</p>
+          <p className='text-sm text-scoreText'>score</p>
+          <p className='font-bold text-4xl text-darkText'>12</p>
           </div>
         </div>
       </header>
-    </div>
+      <div className='w-[50%] m-auto flex items-center justify-center bg-triangle bg-no-repeat bg-center bg-[length:200px] mt-20 relative '>
+        
+        <div>
+
+        <div className='m-auto flex space-x-10  items-center' >
+          <div className='bg-white lg:w-32 lg:h-32 w-24 h-24  rounded-full flex items-center border-[10px] justify-center border-cyanStart mt-[-20px]'>
+            <img src={paper} alt="paper" className='md:w-8 w-8'/>
+          </div>
+          <div className='bg-white  lg:w-32 lg:h-32 w-24 h-24 rounded-full flex items-center justify-center border-[10px] border-scissorsStart  mt-[-20px] '>
+            <img src={scissors} alt="scissors" className='m-7 w-8' />
+          </div>
+        </div>
+        <div className='bg-white lg:w-32 lg:h-32 w-24 h-24  rounded-full flex items-center  justify-center border-[10px] border-rockStart ml-16 mt-10 lg:ml-24 '>
+            <img src={rock} alt="scissors" className='m-7 w-8' />
+        </div>
+        </div>
+      </div>
+      <div className='flex items-center justify-center md:justify-end py-20 px-6'>
+        <button className="border-white border-2 px-5 capitalize py-1 text-white">reset</button>
+      </div>
+      
+    </>
   )
 }
 
